01e90cd0414e98b790f8a1284fb252b0ce84073a
Exote <m.howebrowne@gmail.com>
2020-05-30 19:04:54 +0100

    remove unused settings

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 function findPath() {}
 
 function config() {
-  game.settings.register("route-finder", "snapToGrid", {
-    name: game.i18n.localize("route-finder.snapToGrid.name"),
-    hint: game.i18n.localize("route-finder.snapToGrid.hint"),
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean,
-  });
-
-  game.settings.register("route-finder", "reportMoveToChat", {
-    name: game.i18n.localize("route-finder.reportMoveToChat.name"),
-    hint: game.i18n.localize("route-finder.reportMoveToChat.hint"),
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean,
-  });
-
-  game.settings.register("route-finder", "strokeWidth", {
-    name: game.i18n.localize("route-finder.strokeWidth.name"),
-    hint: game.i18n.localize("route-finder.strokeWidth.hint"),
-    scope: "world",
-    config: true,
-    default: 12,
-    type: Number,
-  });
-
-  game.settings.register("route-finder", "fontSize", {
-    name: game.i18n.localize("route-finder.fontSize.name"),
-    hint: game.i18n.localize("route-finder.fontSize.hint"),
-    scope: "world",
-    config: true,
-    default: 32,
-    type: Number,
-  });
-
   game.settings.register("route-finder", "allowDiagonals", {
     name: game.i18n.localize("route-finder.allowDiagonals.name"),
     hint: game.i18n.localize("route-finder.allowDiagonals.hint"),
@@ -62,81 +26,6 @@ function config() {
     config: true,
     default: 5,
     type: Number,
-  });
-
-  game.settings.register("route-finder", "friendlyBlocksFriendly", {
-    name: game.i18n.localize("route-finder.friendlyBlocksFriendly.name"),
-    hint: game.i18n.localize("route-finder.friendlyBlocksFriendly.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "friendlyBlocksNeutral", {
-    name: game.i18n.localize("route-finder.friendlyBlocksNeutral.name"),
-    hint: game.i18n.localize("route-finder.friendlyBlocksNeutral.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "friendlyBlocksHostile", {
-    name: game.i18n.localize("route-finder.friendlyBlocksHostile.name"),
-    hint: game.i18n.localize("route-finder.friendlyBlocksHostile.hint"),
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean,
-  });
-
-  game.settings.register("route-finder", "neutralBlocksFriendly", {
-    name: game.i18n.localize("route-finder.neutralBlocksFriendly.name"),
-    hint: game.i18n.localize("route-finder.neutralBlocksFriendly.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "neutralBlocksNeutral", {
-    name: game.i18n.localize("route-finder.neutralBlocksNeutral.name"),
-    hint: game.i18n.localize("route-finder.neutralBlocksNeutral.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "neutralBlocksHostile", {
-    name: game.i18n.localize("route-finder.neutralBlocksHostile.name"),
-    hint: game.i18n.localize("route-finder.neutralBlocksHostile.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-
-  game.settings.register("route-finder", "hostileBlocksFriendly", {
-    name: game.i18n.localize("route-finder.hostileBlocksFriendly.name"),
-    hint: game.i18n.localize("route-finder.hostileBlocksFriendly.hint"),
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "hostileBlocksNeutral", {
-    name: game.i18n.localize("route-finder.hostileBlocksNeutral.name"),
-    hint: game.i18n.localize("route-finder.hostileBlocksNeutral.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-  });
-  game.settings.register("route-finder", "hostileBlocksHostile", {
-    name: game.i18n.localize("route-finder.hostileBlocksHostile.name"),
-    hint: game.i18n.localize("route-finder.hostileBlocksHostile.hint"),
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
   });
 }
 
